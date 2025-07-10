fe6b1f20835dc1bdf47e37d2f600553246480f32
Eyver Valbuena <eyver9720@gmail.com>
2025-07-10 10:59:18 -0500

    creacioón automatica de las tablas segun modelo

---
+++ crear_tablas.py
@@ -0,0 +1,14 @@
+# Importar los modelos para registrarlos antes de crear las tablas
+from modelo import base
+from modelo import finca
+from modelo import usuario
+from modelo import semoviente
+from modelo import sanitario
+from modelo import almacen
+from modelo import alerta
+
+# importar la función que crea las tablas
+from modelo.base import crear_tablas
+
+# creación de tablas
+crear_tablas()
