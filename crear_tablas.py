0d82d28cfaa53c8ea42bff025fce32de7862daf0
Eyver Valbuena <eyver9720@gmail.com>
2025-07-11 09:28:04 -0500

    corrección de creacion de tablas

--- crear_tablas.py
+++ crear_tablas.py
@@ -1,11 +1,13 @@
 # Importar los modelos para registrarlos antes de crear las tablas
-from modelo import base
+from config import engine
+from modelo.base import crear_tablas
+
 from modelo import finca
-from modelo import usuario
-from modelo import semoviente
-from modelo import sanitario
-from modelo import almacen
-from modelo import alerta
+#from modelo import usuario
+#from modelo import semoviente
+#from modelo import sanitario
+#from modelo import almacen
+#from modelo import alerta
 
 # importar la función que crea las tablas
 from modelo.base import crear_tablas
